64236df418aec176e7503da821e60b720bbd4bb3
Tkacz <gabrielmt2@al.insper.edu.br>
2021-08-27 22:10:04 -0300

    finalizando labs 1 e 2

--- Lab2-PIO-Driver/src/main.c
+++ Lab2-PIO-Driver/src/main.c
@@ -19,11 +19,16 @@
 
 #include "asf.h"
 
+/************************************************************************/
+/* defines                                                              */
+/************************************************************************/
+       
 #define LED_PIO           PIOC                 // periferico que controla o LED
 #define ID_PIOC        12                  // ID do periférico PIOC (controla LED)
 #define LED_PIO_IDX       8                    // ID do LED no PIO
 #define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
 #define LED_PIO_ID  ID_PIOC  // ID do periférico PIOC (controla LED)
+
 
 // Configuracoes do botao
 #define BUT_PIO          PIOA
@@ -32,6 +37,14 @@
 #define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
 
 
+/*  Default pin configuration (no attribute). */
+#define _PIO_DEFAULT             (0u << 0)
+/*  The internal pin pull-up is active. */
+#define _PIO_PULLUP              (1u << 0)
+/*  The internal glitch filter is active. */
+#define _PIO_DEGLITCH            (1u << 1)
+/*  The internal debouncing filter is active. */
+#define _PIO_DEBOUNCE            (1u << 3)
 
 /************************************************************************/
 /* constants                                                            */
@@ -54,6 +67,68 @@ void init(void);
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
+void _pio_set(Pio *p_pio, const uint32_t ul_mask){
+	p_pio->PIO_SODR = ul_mask;
+}
+
+void _pio_clear(Pio *p_pio, const uint32_t ul_mask){
+	p_pio->PIO_CODR = ul_mask;
+}
+
+void _pio_pull_up(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_pull_up_enable){	
+	if (ul_pull_up_enable){
+		p_pio->PIO_PUER = ul_mask;
+	}
+	else{
+		p_pio->PIO_PUDR = ul_mask;
+	}
+}
+
+void _pio_set_input(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_attribute){
+	// Desabilita registrador 
+	p_pio->PIO_ODR = ul_mask;
+	
+	// Checa debounce e deglitch
+	if (ul_attribute & (_PIO_DEGLITCH | _PIO_DEBOUNCE)){
+		// The glitch filters are controlled by the Input Filter Enable Register
+		p_pio->PIO_IFER = ul_mask;
+	}
+	else{
+		// Input Filter Disable Register
+		p_pio->PIO_IFDR = ul_mask;
+	}
+	
+	// Checa pull up
+	if (ul_attribute & _PIO_PULLUP){
+		_pio_pull_up(p_pio, ul_mask, 1);
+	}
+}
+
+void _pio_set_output(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_default_level, 
+					const uint32_t ul_multidrive_enable, const uint32_t ul_pull_up_enable){
+	p_pio->PIO_PER = ul_mask;
+	
+	p_pio->PIO_OER = ul_mask;
+	
+	if (ul_default_level){
+		_pio_set(p_pio, ul_mask);
+	}
+	else{
+		_pio_clear(p_pio, ul_mask);
+	}
+	
+	if (ul_pull_up_enable){
+		_pio_pull_up(p_pio, ul_mask, 1);
+	}	
+	
+	if (ul_multidrive_enable){
+		p_pio->PIO_MDER = ul_mask;
+	} 
+	else{
+		p_pio->PIO_MDDR = ul_mask;
+	}
+}
+
 
 // Função de inicialização do uC
 void init(void){
@@ -68,24 +143,18 @@ void init(void){
 	pmc_enable_periph_clk(LED_PIO_ID);
 	
 	//Inicializa PC8 como saída
-	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	_pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
 	
 	// Inicializa PIO do botao
 	pmc_enable_periph_clk(BUT_PIO_ID);
 	
 	// configura pino ligado ao botão como entrada com um pull-up.
-	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	_pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, _PIO_PULLUP | _PIO_DEBOUNCE);
 	
 	// Ativa o Pull-up, resistor alimentado para vcc
-	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
+	_pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
 	
 }
-
-
-
-/************************************************************************/
-/* Main                                                                 */
-/************************************************************************/
 
 void blink(int n){
 	for (int i = 0; i < n; i++){
@@ -98,28 +167,28 @@ void blink(int n){
 
 // Funcao principal chamada na inicalizacao do uC.
 int main(void){
-	init();
+  init();
 
-	// super loop
-	// aplicacoes embarcadas não devem sair do while(1
-	int pressed = 0;
-	
-	while (1){
-		int input = pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK);
-		int output = pio_get(BUT_PIO, PIO_OUTPUT_0, BUT_PIO_IDX_MASK);
-		
-		if ((output == 0) && (input == 0)){
-			pressed = 1;
-		}
-		else{
-			if(pressed){
-				blink(5);
-				pressed = 0;
-			}
-			else{
-				_pio_set(PIOC, LED_PIO_IDX_MASK);
-			}
-		}
-	}
-	return 0;
+  // super loop
+  // aplicacoes embarcadas não devem sair do while(1
+  int pressed = 0;
+  
+  while (1){
+	  int input = pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK);
+	  int output = pio_get(BUT_PIO, PIO_OUTPUT_0, BUT_PIO_IDX_MASK);
+	  
+	  if ((output == 0) && (input == 0)){		
+		  pressed = 1;
+	  }
+	  else{
+		  if(pressed){
+			  blink(5);
+			  pressed = 0;
+		  }
+		  else{
+			  _pio_set(PIOC, LED_PIO_IDX_MASK);
+		  }
+	  } 
+  }
+  return 0;
 }
